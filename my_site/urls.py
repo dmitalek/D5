764d9b9d3e5f3d771bbcf7ad7daea26b90c924f8
Aleksei <61025540+dmitalek@users.noreply.github.com>
2020-07-06 22:52:18 +0300

    Add files via upload

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,17 +1,18 @@
-
 from django.contrib import admin
-from django.urls import path, include
-from p_library.views import AuthorEdit, AuthorList, author_create_many, books_authors_create_many
+from django.urls import path
+from p_library.views import  AuthorEdit, AuthorList, BookEdit, FriendList, FriendUpdate, FriendEdit, Publisher
 from p_library import views
 
 app_name = 'p_library'
 urlpatterns = [
-        
         path('admin/', admin.site.urls),
-        
-        path('', views.FriendList),
+        path('', views.books_list),
+        path('index/', views.index),
+        path('author/create', AuthorEdit.as_view(), name='author_create'),
         path('author/', AuthorList.as_view(), name='author_list'),
-        
-        
-        path('friend/', views.FriendList),
+        path('book/create', BookEdit.as_view(), name='book_create'),
+        path('friends', FriendList.as_view(), name='friend_list'),
+        path('<pk>/friend_update', FriendUpdate.as_view(), name='friend_update'),
+        path('friend/create', FriendEdit.as_view(), name='friend_create'),
+        path('maker', Publisher.as_view(), name='publish'),
 ]
